5d1cb25e30982efc276b6652e801e539f6ea62e5
gu4rd69 <hossfeldtt@gmail.com>
2026-09-22 19:59:52 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "=== STARTE AUTOMATISCHEN STEAMCMD DOWNLOAD ==="
-/AMP/steamcmd/steamcmd.sh +force_install_dir /AMP/alchemy-factory/4550060 +login anonymous +app_update 4550060 validate +quit
-
-echo "=== DOWNLOAD FERTIG - RECHTE SETZEN ==="
-chmod +x /AMP/alchemy-factory/4550060/AlchemyFactoryServer.exe
-
-echo "=== STARTE WINE SERVER ==="
-/usr/bin/xvfb-run -a wine /AMP/alchemy-factory/4550060/AlchemyFactoryServer.exe -port 7780 -log
